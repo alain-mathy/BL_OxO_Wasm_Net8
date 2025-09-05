c2239149e01bc709d5dab188ed751c9b39c2b2a1
alain-mathy <alain_mathy@hotmail.com>
2025-09-05 11:04:00 +0200

    Add GameHub Add Lobby page Modify MainLayout.razor

---
+++ BL_OxO_Wasm_Net8/BL_OxO_Wasm_Net8/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BL_OxO_Wasm_Net8.Hubs
+{
+    public class GameHub : Hub
+    {
+        public override  Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Player with id '{Context.ConnectionId}' connected");
+            return  base.OnConnectedAsync();
+        }
+    }
+}
--- BL_OxO_Wasm_Net8/BL_OxO_Wasm_Net8/Program.cs
+++ BL_OxO_Wasm_Net8/BL_OxO_Wasm_Net8/Program.cs
@@ -1,11 +1,14 @@
 using BL_OxO_Wasm_Net8.Client.Pages;
 using BL_OxO_Wasm_Net8.Components;
+using BL_OxO_Wasm_Net8.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -25,6 +28,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapHub<GameHub>("/gamehub");
 
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
