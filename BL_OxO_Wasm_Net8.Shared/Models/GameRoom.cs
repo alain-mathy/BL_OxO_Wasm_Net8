8b288a0b03b1661111437e2a823136dcc01cec91
alain-mathy <alain_mathy@hotmail.com>
2025-09-06 08:26:03 +0200

    Add GameRoom entity Modify OnConnectedAsync -> GameHub

---
+++ BL_OxO_Wasm_Net8.Shared/Models/GameRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_OxO_Wasm_Net8.Shared.Models
+{
+    public class GameRoom
+    {
+        public string RoomId { get; set; }
+        public string RoomName { get; set; }
+    }
+}
--- BL_OxO_Wasm_Net8/BL_OxO_Wasm_Net8/Hubs/GameHub.cs
+++ BL_OxO_Wasm_Net8/BL_OxO_Wasm_Net8/Hubs/GameHub.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BL_OxO_Wasm_Net8.Shared.Models;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BL_OxO_Wasm_Net8.Hubs
 {
     public class GameHub : Hub
     {
-        public override  Task OnConnectedAsync()
+        private static readonly List<GameRoom> _rooms = new();
+        public override  async Task OnConnectedAsync()
         {
             Console.WriteLine($"Player with id '{Context.ConnectionId}' connected");
-            return  base.OnConnectedAsync();
+
+            await Clients.Caller.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
         }
     }
 }
